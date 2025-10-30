f9e407c3db8975ced0319be038a451e5abd245ae
mihle2025 <mihletshona3@gmail.com>
2025-10-30 15:13:55 +0200

    java script to github

---
+++ basics.js
@@ -0,0 +1,7 @@
+const firstName = " Mihle "
+const lastName = " Tshona "
+let age = " 22 "
+const fullName = firstName + lastName 
+console.log("My name is " + fullName + " and I am " + age + " years old." )
+age = " 23 "
+console.log("Next year, I will be " + age + " years old.")
---
+++ operation-types.js
@@ -0,0 +1,10 @@
+let number1 = 10
+let number2 = 5
+let number1String = String(number1)
+let sum = number1 + number2
+let product = number1 * number2
+console.log("number 1:", number1)
+console.log("number 2:", number2)
+console.log("number 1 as a string:", number1String)
+console.log("sum:", sum)
+console.log("product:", product)
